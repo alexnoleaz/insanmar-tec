891b20b31cc3fcc86121de5ae1bfe73ccbace65e
Alexander Nole <cnole_azahache@outlook.com>
2024-06-04 21:38:06 -0500

    Initial Setup

--- src/InsanmarTec.WinForms/Program.cs
+++ src/InsanmarTec.WinForms/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+
 namespace InsanmarTec.WinForms
 {
     internal static class Program
@@ -11,7 +16,20 @@ namespace InsanmarTec.WinForms
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(CreateHostBuilder().Build().Services.GetRequiredService<Form1>());
         }
+
+        static IHostBuilder CreateHostBuilder() =>
+            Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration(configuration =>
+                    configuration
+                        .SetBasePath(Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location)!)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true))
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<Form1>();
+                    new Startup(context.Configuration).ConfigureServices(services);
+                });
+
     }
 }
---
+++ src/InsanmarTec.WinForms/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InsanmarTec.WinForms
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration) => _configuration = configuration;
+
+        public void ConfigureServices(IServiceCollection services) { }
+    }
+}
