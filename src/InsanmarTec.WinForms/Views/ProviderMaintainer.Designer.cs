5dfb08d136f2d18294794633b04700b088e712c7
Ashley Gámez <gamezgavidia@gmail.com>
2024-06-21 12:50:48 -0500

    MantenedorProv-2

--- src/InsanmarTec.WinForms/Views/ProviderMaintainer.Designer.cs
+++ src/InsanmarTec.WinForms/Views/ProviderMaintainer.Designer.cs
@@ -31,22 +31,31 @@
             CodPro = new Label();
             textBox1 = new TextBox();
             groupBox1 = new GroupBox();
-            RUCProv = new Label();
-            textBox2 = new TextBox();
-            RazSoc = new Label();
-            Rub = new Label();
-            label1 = new Label();
             textBox3 = new TextBox();
+            label1 = new Label();
+            Rub = new Label();
+            RazSoc = new Label();
+            textBox2 = new TextBox();
+            RUCProv = new Label();
             label2 = new Label();
             textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox8 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            btnGrab = new Button();
             groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // CodPro
             // 
             CodPro.AutoSize = true;
+            CodPro.BackColor = SystemColors.InactiveCaption;
             CodPro.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            CodPro.Location = new Point(17, 31);
+            CodPro.Location = new Point(30, 60);
             CodPro.Name = "CodPro";
             CodPro.Size = new Size(128, 16);
             CodPro.TabIndex = 0;
@@ -54,13 +63,21 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(151, 29);
+            textBox1.Location = new Point(291, 277);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(176, 23);
+            textBox1.Size = new Size(271, 23);
             textBox1.TabIndex = 1;
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(btnGrab);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(textBox5);
             groupBox1.Controls.Add(textBox4);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(textBox3);
@@ -71,83 +88,155 @@
             groupBox1.Controls.Add(RUCProv);
             groupBox1.Controls.Add(CodPro);
             groupBox1.Controls.Add(textBox1);
-            groupBox1.Location = new Point(12, 3);
+            groupBox1.Location = new Point(12, 6);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(612, 225);
+            groupBox1.Size = new Size(612, 431);
             groupBox1.TabIndex = 3;
             groupBox1.TabStop = false;
             groupBox1.Text = "Datos del Proveedor";
             // 
-            // RUCProv
+            // textBox3
             // 
-            RUCProv.AutoSize = true;
-            RUCProv.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            RUCProv.Location = new Point(370, 31);
-            RUCProv.Name = "RUCProv";
-            RUCProv.Size = new Size(38, 16);
-            RUCProv.TabIndex = 2;
-            RUCProv.Text = "RUC:";
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(414, 29);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(176, 23);
-            textBox2.TabIndex = 3;
-            // 
-            // RazSoc
-            // 
-            RazSoc.AutoSize = true;
-            RazSoc.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            RazSoc.Location = new Point(17, 114);
-            RazSoc.Name = "RazSoc";
-            RazSoc.Size = new Size(93, 16);
-            RazSoc.TabIndex = 4;
-            RazSoc.Text = "Razon Social:";
-            // 
-            // Rub
-            // 
-            Rub.AutoSize = true;
-            Rub.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            Rub.Location = new Point(17, 159);
-            Rub.Name = "Rub";
-            Rub.Size = new Size(47, 16);
-            Rub.TabIndex = 5;
-            Rub.Text = "Rubro";
+            textBox3.Location = new Point(30, 211);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(224, 23);
+            textBox3.TabIndex = 7;
             // 
             // label1
             // 
             label1.AutoSize = true;
+            label1.BackColor = SystemColors.InactiveCaption;
             label1.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(17, 71);
+            label1.Location = new Point(291, 60);
             label1.Name = "label1";
             label1.Size = new Size(130, 16);
             label1.TabIndex = 6;
             label1.Text = "Nombre Completo:";
             // 
-            // textBox3
+            // Rub
             // 
-            textBox3.Location = new Point(151, 58);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(176, 23);
-            textBox3.TabIndex = 7;
+            Rub.AutoSize = true;
+            Rub.BackColor = SystemColors.InactiveCaption;
+            Rub.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Rub.Location = new Point(291, 192);
+            Rub.Name = "Rub";
+            Rub.Size = new Size(47, 16);
+            Rub.TabIndex = 5;
+            Rub.Text = "Rubro";
+            // 
+            // RazSoc
+            // 
+            RazSoc.AutoSize = true;
+            RazSoc.BackColor = SystemColors.InactiveCaption;
+            RazSoc.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RazSoc.Location = new Point(30, 258);
+            RazSoc.Name = "RazSoc";
+            RazSoc.Size = new Size(93, 16);
+            RazSoc.TabIndex = 4;
+            RazSoc.Text = "Razon Social:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(291, 145);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(271, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // RUCProv
+            // 
+            RUCProv.AutoSize = true;
+            RUCProv.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RUCProv.Location = new Point(30, 192);
+            RUCProv.Name = "RUCProv";
+            RUCProv.Size = new Size(38, 16);
+            RUCProv.TabIndex = 2;
+            RUCProv.Text = "RUC:";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.BackColor = SystemColors.InactiveCaption;
             label2.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label2.Location = new Point(370, 78);
+            label2.Location = new Point(291, 126);
             label2.Name = "label2";
-            label2.Size = new Size(35, 16);
+            label2.Size = new Size(73, 16);
             label2.TabIndex = 8;
-            label2.Text = "DNI:";
+            label2.Text = "Dirección:";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(411, 76);
+            textBox4.Location = new Point(30, 145);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(176, 23);
             textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(30, 79);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(170, 23);
+            textBox5.TabIndex = 10;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(291, 79);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(271, 23);
+            textBox6.TabIndex = 11;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = SystemColors.InactiveCaption;
+            label3.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(30, 126);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 16);
+            label3.TabIndex = 12;
+            label3.Text = "Teléfono:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Lucida Fax", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(291, 258);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 16);
+            label4.TabIndex = 13;
+            label4.Text = "Correo electrónico:";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(30, 277);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(209, 23);
+            textBox8.TabIndex = 15;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(351, 0);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(234, 23);
+            dateTimePicker1.TabIndex = 16;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(291, 211);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(176, 23);
+            comboBox1.TabIndex = 17;
+            // 
+            // btnGrab
+            // 
+            btnGrab.BackColor = SystemColors.InactiveCaption;
+            btnGrab.Font = new Font("Lucida Fax", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnGrab.Location = new Point(246, 345);
+            btnGrab.Name = "btnGrab";
+            btnGrab.Size = new Size(118, 54);
+            btnGrab.TabIndex = 18;
+            btnGrab.Text = "Grabar";
+            btnGrab.UseVisualStyleBackColor = false;
             // 
             // ProviderMaintainer
             // 
@@ -173,10 +262,18 @@
         private Label RUCProv;
         private Label RazSoc;
         private TextBox textBox2;
-        private TextBox textBox4;
-        private Label label2;
         private TextBox textBox3;
         private Label label1;
         private Label Rub;
+        private Label label2;
+        private TextBox textBox4;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private Label label4;
+        private Label label3;
+        private TextBox textBox8;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private Button btnGrab;
     }
 }
