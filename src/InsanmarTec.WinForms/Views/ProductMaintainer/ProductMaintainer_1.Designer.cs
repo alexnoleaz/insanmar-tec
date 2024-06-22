304b24d60580ab4c9472467c5ca75015f56b692f
Ashley Gámez <gamezgavidia@gmail.com>
2024-06-22 02:47:37 -0500

    MantenedorProd1

---
+++ src/InsanmarTec.WinForms/Views/ProductMaintainer/ProductMaintainer_1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace InsanmarTec.WinForms.Views.ProductMaintainer
+{
+    partial class ProductMaintainer_1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            comboBox4 = new ComboBox();
+            comboBox3 = new ComboBox();
+            comboBox2 = new ComboBox();
+            textBox3 = new TextBox();
+            label10 = new Label();
+            textBox4 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label9 = new Label();
+            label8 = new Label();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            comboBox1 = new ComboBox();
+            domainUpDown1 = new DomainUpDown();
+            dateTimePicker1 = new DateTimePicker();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(domainUpDown1);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(comboBox4);
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Lucida Fax", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(596, 379);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Detalles de Productos";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(24, 268);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(234, 25);
+            comboBox4.TabIndex = 18;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(304, 194);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(235, 25);
+            comboBox3.TabIndex = 17;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(24, 194);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(234, 25);
+            comboBox2.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(304, 130);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(235, 25);
+            textBox3.TabIndex = 14;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(304, 110);
+            label10.Name = "label10";
+            label10.Size = new Size(103, 17);
+            label10.TabIndex = 13;
+            label10.Text = "Descripción:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(24, 342);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(119, 25);
+            textBox4.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(24, 56);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(234, 25);
+            textBox2.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(304, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(235, 25);
+            textBox1.TabIndex = 9;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(233, 322);
+            label9.Name = "label9";
+            label9.Size = new Size(174, 17);
+            label9.TabIndex = 8;
+            label9.Text = "Fecha de Adquisición:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(24, 322);
+            label8.Name = "label8";
+            label8.Size = new Size(142, 17);
+            label8.TabIndex = 7;
+            label8.Text = "Precio de Compra";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(304, 248);
+            label7.Name = "label7";
+            label7.Size = new Size(81, 17);
+            label7.TabIndex = 6;
+            label7.Text = "Cantidad:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(24, 248);
+            label6.Name = "label6";
+            label6.Size = new Size(132, 17);
+            label6.TabIndex = 5;
+            label6.Text = "Tipo de Calidad:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(304, 174);
+            label5.Name = "label5";
+            label5.Size = new Size(129, 17);
+            label5.TabIndex = 4;
+            label5.Text = "Tipo de Modelo:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(24, 174);
+            label4.Name = "label4";
+            label4.Size = new Size(119, 17);
+            label4.TabIndex = 3;
+            label4.Text = "Tipo de Marca:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(24, 110);
+            label3.Name = "label3";
+            label3.Size = new Size(143, 17);
+            label3.TabIndex = 2;
+            label3.Text = "Tipo de Producto:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(304, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 17);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre del Producto:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(167, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Codigo del Producto:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(24, 130);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(234, 25);
+            comboBox1.TabIndex = 19;
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Location = new Point(304, 269);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(103, 25);
+            domainUpDown1.TabIndex = 20;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(233, 342);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(319, 25);
+            dateTimePicker1.TabIndex = 21;
+            // 
+            // ProductMaintainer_1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(620, 410);
+            Controls.Add(groupBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ProductMaintainer_1";
+            Text = "ProductMaintainer_1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label label5;
+        private TextBox textBox4;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private TextBox textBox3;
+        private Label label10;
+        private ComboBox comboBox4;
+        private DomainUpDown domainUpDown1;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+    }
+}
---
+++ src/InsanmarTec.WinForms/Views/ProductMaintainer/ProductMaintainer_1.cs
@@ -0,0 +1,27 @@
+﻿using InsanmarTec.Domain.Shared.Dependency;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InsanmarTec.WinForms.Views.ProductMaintainer
+{
+    public partial class ProductMaintainer_1 : Form, ITransientDependency
+    {
+        public ProductMaintainer_1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
