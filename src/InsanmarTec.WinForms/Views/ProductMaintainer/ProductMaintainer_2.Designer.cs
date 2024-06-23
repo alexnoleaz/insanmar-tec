a9b4ab4977597de545e707df800604a60b91d9c4
Ashley Gámez <gamezgavidia@gmail.com>
2024-06-22 22:34:49 -0500

    MantenedorProd2

---
+++ src/InsanmarTec.WinForms/Views/ProductMaintainer/ProductMaintainer_2.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace InsanmarTec.WinForms.Views.ProductMaintainer
+{
+    partial class ProductMaintainer_2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Lucida Fax", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(146, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(284, 25);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "Ingrese el codigo de producto";
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.InactiveCaption;
+            button1.Font = new Font("Lucida Fax", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(457, 7);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 53);
+            button1.TabIndex = 1;
+            button1.Text = "Buscar";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 68);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(580, 276);
+            dataGridView1.TabIndex = 3;
+            // 
+            // ProductMaintainer_2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(604, 371);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ProductMaintainer_2";
+            Text = "ProductMaintainer_2";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private DataGridView dataGridView1;
+    }
+}
