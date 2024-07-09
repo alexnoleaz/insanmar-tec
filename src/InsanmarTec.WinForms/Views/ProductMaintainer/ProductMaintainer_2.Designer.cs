264173ae7effb9cb52d9999fcacc610d78218443
Ashly NGG <gamezgavidia@gmail.com>
2024-07-09 15:14:17 -0500

    _

--- src/InsanmarTec.WinForms/Views/ProductMaintainer/ProductMaintainer_2.Designer.cs
+++ src/InsanmarTec.WinForms/Views/ProductMaintainer/ProductMaintainer_2.Designer.cs
@@ -28,62 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            button1 = new Button();
-            dataGridView1 = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            btnAdd = new FontAwesome.Sharp.IconButton();
+            btnSearch = new FontAwesome.Sharp.IconButton();
+            txtSearch = new MaterialSkin.Controls.MaterialTextBox();
             SuspendLayout();
             // 
-            // textBox1
+            // btnAdd
             // 
-            textBox1.Font = new Font("Lucida Fax", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.Location = new Point(146, 22);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(284, 25);
-            textBox1.TabIndex = 0;
-            textBox1.Text = "Ingrese el codigo de producto";
+            btnAdd.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnAdd.BackColor = Color.FromArgb(8, 86, 207);
+            btnAdd.Cursor = Cursors.Hand;
+            btnAdd.FlatAppearance.BorderSize = 0;
+            btnAdd.FlatAppearance.MouseDownBackColor = Color.FromArgb(5, 60, 140);
+            btnAdd.FlatAppearance.MouseOverBackColor = Color.FromArgb(30, 100, 190);
+            btnAdd.FlatStyle = FlatStyle.Flat;
+            btnAdd.Font = new Font("Segoe UI Historic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnAdd.ForeColor = Color.White;
+            btnAdd.IconChar = FontAwesome.Sharp.IconChar.Plus;
+            btnAdd.IconColor = Color.White;
+            btnAdd.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnAdd.IconSize = 32;
+            btnAdd.ImageAlign = ContentAlignment.MiddleLeft;
+            btnAdd.Location = new Point(466, -36);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(109, 36);
+            btnAdd.TabIndex = 13;
+            btnAdd.Text = "Agregar";
+            btnAdd.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnAdd.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // btnSearch
             // 
-            button1.BackColor = SystemColors.InactiveCaption;
-            button1.Font = new Font("Lucida Fax", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(457, 7);
-            button1.Name = "button1";
-            button1.Size = new Size(87, 53);
-            button1.TabIndex = 1;
-            button1.Text = "Buscar";
-            button1.UseVisualStyleBackColor = false;
+            btnSearch.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            btnSearch.BackColor = Color.FromArgb(75, 146, 255);
+            btnSearch.Cursor = Cursors.Hand;
+            btnSearch.FlatAppearance.BorderSize = 0;
+            btnSearch.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 120, 215);
+            btnSearch.FlatAppearance.MouseOverBackColor = Color.FromArgb(95, 166, 255);
+            btnSearch.FlatStyle = FlatStyle.Flat;
+            btnSearch.IconChar = FontAwesome.Sharp.IconChar.Search;
+            btnSearch.IconColor = Color.White;
+            btnSearch.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            btnSearch.IconSize = 32;
+            btnSearch.Location = new Point(419, -36);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(41, 36);
+            btnSearch.TabIndex = 12;
+            btnSearch.UseVisualStyleBackColor = false;
             // 
-            // dataGridView1
+            // txtSearch
             // 
-            dataGridView1.AllowUserToOrderColumns = true;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 68);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(580, 276);
-            dataGridView1.TabIndex = 3;
+            txtSearch.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtSearch.AnimateReadOnly = false;
+            txtSearch.BorderStyle = BorderStyle.None;
+            txtSearch.Depth = 0;
+            txtSearch.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            txtSearch.Hint = "Buscar marca por nombre...";
+            txtSearch.LeadingIcon = null;
+            txtSearch.Location = new Point(30, -36);
+            txtSearch.MaxLength = 50;
+            txtSearch.MouseState = MaterialSkin.MouseState.OUT;
+            txtSearch.Multiline = false;
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(391, 36);
+            txtSearch.TabIndex = 11;
+            txtSearch.Text = "";
+            txtSearch.TrailingIcon = null;
+            txtSearch.UseTallSize = false;
             // 
             // ProductMaintainer_2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(604, 371);
-            Controls.Add(dataGridView1);
-            Controls.Add(button1);
-            Controls.Add(textBox1);
+            BackColor = Color.White;
+            ClientSize = new Size(604, 431);
+            Controls.Add(btnAdd);
+            Controls.Add(btnSearch);
+            Controls.Add(txtSearch);
             FormBorderStyle = FormBorderStyle.None;
             Name = "ProductMaintainer_2";
             Text = "ProductMaintainer_2";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private TextBox textBox1;
-        private Button button1;
-        private DataGridView dataGridView1;
+        private FontAwesome.Sharp.IconButton btnAdd;
+        private FontAwesome.Sharp.IconButton btnSearch;
+        private MaterialSkin.Controls.MaterialTextBox txtSearch;
     }
 }
