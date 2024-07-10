8fbec929aeb284b5c22b599153eadf19f68b2dcc
Ashly NGG <gamezgavidia@gmail.com>
2024-07-10 05:38:38 -0500

    ProviderMaintainer_final

--- src/InsanmarTec.WinForms/Views/ProviderMaintainer/ProviderMaintainer_2.Designer.cs
+++ src/InsanmarTec.WinForms/Views/ProviderMaintainer/ProviderMaintainer_2.Designer.cs
@@ -28,61 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            txbBus = new TextBox();
-            button1 = new Button();
-            datGrViProv = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)datGrViProv).BeginInit();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
+            dgvProducts = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).BeginInit();
             SuspendLayout();
             // 
-            // txbBus
+            // iconButton1
             // 
-            txbBus.Font = new Font("Lucida Fax", 11.25F);
-            txbBus.Location = new Point(125, 28);
-            txbBus.Name = "txbBus";
-            txbBus.Size = new Size(311, 25);
-            txbBus.TabIndex = 4;
-            txbBus.Text = "Ingrese el codigo a buscar";
+            iconButton1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            iconButton1.BackColor = Color.FromArgb(8, 86, 207);
+            iconButton1.Cursor = Cursors.Hand;
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatAppearance.MouseDownBackColor = Color.FromArgb(5, 60, 140);
+            iconButton1.FlatAppearance.MouseOverBackColor = Color.FromArgb(30, 100, 190);
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.Font = new Font("Segoe UI Historic", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            iconButton1.ForeColor = Color.White;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.Plus;
+            iconButton1.IconColor = Color.White;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.IconSize = 32;
+            iconButton1.ImageAlign = ContentAlignment.MiddleLeft;
+            iconButton1.Location = new Point(477, 2);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(109, 36);
+            iconButton1.TabIndex = 25;
+            iconButton1.Text = "Agregar";
+            iconButton1.TextImageRelation = TextImageRelation.ImageBeforeText;
+            iconButton1.UseVisualStyleBackColor = false;
             // 
-            // button1
+            // iconButton2
             // 
-            button1.BackColor = SystemColors.InactiveCaption;
-            button1.Font = new Font("Lucida Fax", 11.25F);
-            button1.Location = new Point(453, 6);
-            button1.Name = "button1";
-            button1.Size = new Size(136, 66);
-            button1.TabIndex = 5;
-            button1.Text = "Buscar";
-            button1.UseVisualStyleBackColor = false;
+            iconButton2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            iconButton2.BackColor = Color.FromArgb(75, 146, 255);
+            iconButton2.Cursor = Cursors.Hand;
+            iconButton2.FlatAppearance.BorderSize = 0;
+            iconButton2.FlatAppearance.MouseDownBackColor = Color.FromArgb(60, 120, 215);
+            iconButton2.FlatAppearance.MouseOverBackColor = Color.FromArgb(95, 166, 255);
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.Search;
+            iconButton2.IconColor = Color.White;
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.IconSize = 32;
+            iconButton2.Location = new Point(430, 2);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(41, 36);
+            iconButton2.TabIndex = 24;
+            iconButton2.UseVisualStyleBackColor = false;
             // 
-            // datGrViProv
+            // materialTextBox1
             // 
-            datGrViProv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            datGrViProv.Location = new Point(21, 82);
-            datGrViProv.Name = "datGrViProv";
-            datGrViProv.Size = new Size(568, 304);
-            datGrViProv.TabIndex = 6;
-            datGrViProv.CellContentClick += datGrViProv_CellContentClick;
+            materialTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            materialTextBox1.AnimateReadOnly = false;
+            materialTextBox1.BorderStyle = BorderStyle.None;
+            materialTextBox1.Depth = 0;
+            materialTextBox1.Font = new Font("Microsoft Sans Serif", 16F, FontStyle.Regular, GraphicsUnit.Pixel);
+            materialTextBox1.Hint = "Buscar proveedor por ID...";
+            materialTextBox1.LeadingIcon = null;
+            materialTextBox1.Location = new Point(33, 2);
+            materialTextBox1.MaxLength = 50;
+            materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
+            materialTextBox1.Multiline = false;
+            materialTextBox1.Name = "materialTextBox1";
+            materialTextBox1.Size = new Size(391, 36);
+            materialTextBox1.TabIndex = 23;
+            materialTextBox1.Text = "";
+            materialTextBox1.TrailingIcon = null;
+            materialTextBox1.UseTallSize = false;
+            // 
+            // dgvProducts
+            // 
+            dgvProducts.AllowUserToAddRows = false;
+            dgvProducts.AllowUserToDeleteRows = false;
+            dgvProducts.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dgvProducts.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvProducts.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvProducts.BackgroundColor = Color.White;
+            dgvProducts.BorderStyle = BorderStyle.None;
+            dgvProducts.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dgvProducts.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(20, 45, 85);
+            dataGridViewCellStyle1.Font = new Font("Century Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(8, 86, 207);
+            dataGridViewCellStyle1.SelectionForeColor = Color.White;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dgvProducts.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dgvProducts.ColumnHeadersHeight = 30;
+            dgvProducts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dgvProducts.EnableHeadersVisualStyles = false;
+            dgvProducts.GridColor = Color.FromArgb(20, 45, 85);
+            dgvProducts.Location = new Point(42, 58);
+            dgvProducts.Name = "dgvProducts";
+            dgvProducts.ReadOnly = true;
+            dgvProducts.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dgvProducts.RowHeadersVisible = false;
+            dataGridViewCellStyle2.BackColor = Color.White;
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(20, 45, 85);
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(8, 86, 207);
+            dgvProducts.RowsDefaultCellStyle = dataGridViewCellStyle2;
+            dgvProducts.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dgvProducts.Size = new Size(545, 351);
+            dgvProducts.TabIndex = 22;
             // 
             // ProviderMaintainer_2
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaption;
+            BackColor = SystemColors.ButtonHighlight;
             ClientSize = new Size(620, 410);
-            Controls.Add(datGrViProv);
-            Controls.Add(button1);
-            Controls.Add(txbBus);
+            Controls.Add(iconButton1);
+            Controls.Add(iconButton2);
+            Controls.Add(materialTextBox1);
+            Controls.Add(dgvProducts);
             FormBorderStyle = FormBorderStyle.None;
             Name = "ProviderMaintainer_2";
             Text = "ProviderMaintainer_2";
-            ((System.ComponentModel.ISupportInitialize)datGrViProv).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvProducts).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
-        private TextBox txbBus;
-        private Button button1;
-        private DataGridView datGrViProv;
+
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
+        private DataGridView dgvProducts;
     }
 }
